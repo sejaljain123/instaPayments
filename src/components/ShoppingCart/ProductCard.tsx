fcfe85e14e2e8806932418ebfe7d98e6d0db5ee1
Sejal Jain <sejaljain2043@gmail.com>
2024-03-28 23:07:50 +0530

    convert to taBLE

--- src/components/ShoppingCart/ProductCard.tsx
+++ src/components/ShoppingCart/ProductCard.tsx
@@ -1,47 +1,40 @@
-import { removeItem, updateQuantity } from "@/lib/reducers/checkoutSlice";
-import { Flex, HStack, AspectRatio, VStack, Text, Image, IconButton, Box, Icon } from "@chakra-ui/react";
-import { IoClose } from "react-icons/io5";
-import { PiMinusBold, PiPlusBold } from "react-icons/pi";
-import { useDispatch } from "react-redux";
+import { Tr, Td, IconButton, AspectRatio, Text, Flex, Image, Icon } from '@chakra-ui/react';
+import { PiMinusBold, PiPlusBold } from 'react-icons/pi';
+import { IoClose } from 'react-icons/io5';
+import { useDispatch } from 'react-redux';
+import { updateQuantity, removeItem } from '@/lib/reducers/checkoutSlice';
 
-export default function ProductCard({ item }: { item: any }) {
-
+export default function ProductTableRow({ item }) {
     const dispatch = useDispatch();
 
     const handleRemove = () => {
-        console.log(item.id);
         dispatch(removeItem({ id: item.id }));
-    }
+    };
 
     return (
-        <Flex width="full" justifyContent="space-between" py="4">
-            <HStack width="full" justifyContent="space-between">
-                <HStack spacing="2" w="40%">
-                    <Icon as={IoClose} color="foreground" onClick={handleRemove} cursor="pointer" />
-                    <Flex gap="2" alignItems="center" direction={{ base: "column", md: "row" }} flex="1">
-                        <AspectRatio ratio={1} width={{ base: "12", md: "20" }} position="relative" >
-                            <Image src={item.image} alt="empty-cart" />
-                        </AspectRatio>
-                        <Text color="foreground" fontSize={{ base: "xs", xl: "md" }} w={{ base: "100%", xl: "70%" }} title={item.title} textAlign={{ base: "center", md: "left" }}>{item.title}</Text>
-                    </Flex>
-                </HStack>
-                <VStack spacing="2" w="20%">
-                    <Text color="foreground" fontSize={{ base: "xs", md: "md" }} >Qty</Text>
-                    <HStack spacing="2" w="full" justifyContent="center">
-                        <IconButton icon={<PiMinusBold size="8" />} variant="outline" color="foreground" aria-label="add" size="xs" onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))} />
-                        <Text color="foreground" fontSize={{ base: "xs", md: "md" }} > {item.quantity}</Text>
-                        <IconButton icon={<PiPlusBold size="8" />} variant="outline" color="foreground" aria-label="close" size="xs" onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))} />
-                    </HStack>
-                </VStack>
-                <VStack spacing="2" w="20%">
-                    <Text color="foreground" fontSize={{ base: "xs", md: "md" }} >Price</Text>
-                    <Text color="foreground" fontSize={{ base: "sm", md: "md" }} >${item.price.toFixed(2)}</Text>
-                </VStack>
-                <VStack spacing="2" w="20%">
-                    <Text color="foreground" fontSize={{ base: "xs", md: "md" }} >Total Amount</Text>
-                    <Text color="foreground" fontSize={{ base: "sm", md: "md" }} >${(item.price * item.quantity).toFixed(2)}</Text>
-                </VStack>
-            </HStack>
-        </Flex >
+        <Tr color="foreground" fontSize={{ base: "sm", md: "md" }}>
+            <Td>
+                <Icon as={IoClose} color="foreground" onClick={handleRemove} cursor="pointer" />
+            </Td>
+            <Td>
+                <Flex alignItems="center" gap="2" flexDirection={{ base: "column", md: "row" }}>
+                    <AspectRatio ratio={1} width={{ base: "12", md: "20" }} position="relative">
+                        <Image src={item.image} alt="Product Image" />
+                    </AspectRatio>
+                    <Text color="foreground" fontSize={{ base: "xs", xl: "md" }} w={{ base: "20ch", xl: "50%" }} isTruncated title={item.title} textAlign={{ base: "center", md: "left" }}>
+                        {item.title}
+                    </Text>
+                </Flex>
+            </Td>
+            <Td>
+                <Flex gap="2">
+                    <IconButton icon={<PiMinusBold />} variant="outline" color="foreground" aria-label="Reduce Quantity" size="xs" onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity - 1 }))} />
+                    <Text color="foreground" fontSize={{ base: "xs", md: "md" }}>{item.quantity}</Text>
+                    <IconButton icon={<PiPlusBold />} variant="outline" color="foreground" aria-label="Increase Quantity" size="xs" onClick={() => dispatch(updateQuantity({ id: item.id, quantity: item.quantity + 1 }))} />
+                </Flex>
+            </Td>
+            <Td>${item.price.toFixed(2)}</Td>
+            <Td>${(item.price * item.quantity).toFixed(2)}</Td>
+        </Tr>
     );
 }
--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,20 +1,29 @@
-import { Box, Flex, Text, Image, Button, HStack, VStack, AspectRatio, Input, Divider } from '@chakra-ui/react';
+import { Box, Flex, Text, Image, Button, HStack, VStack, AspectRatio, Input, Divider, Table, Tbody, Tr, Th, Td, Thead, TableContainer } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import EmptyCart from './EmptyCart';
 import ProductCard from './ProductCard';
 
 export default function ShoppingCart({ products }: any) {
-
     return (
         <Box w="full" h="full" display="flex" flexDirection="column" justifyContent="space-between" alignItems="center" mt="4">
-            <VStack spacing="4" alignItems="flex-start" justifyContent="flex-start" width="full"   >
-                {products.map((item: any) => (
-                    <Box w="full" key={item.id}>
-                        <ProductCard item={item} />
-                        <Divider />
-                    </Box>
-                ))}
-            </VStack>
-        </Box >
+            <TableContainer width="full">
+                <Table variant="simple" size="sm" width="full">
+                    <Thead>
+                        <Tr>
+                            <Th></Th>
+                            <Th>Item</Th>
+                            <Th>Qty</Th>
+                            <Th>Price</Th>
+                            <Th>Total</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody mt="2">
+                        {products.map((item: any) => (
+                            <ProductCard item={item} key={item.id} />
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
     );
 }
